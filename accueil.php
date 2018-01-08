6526329f66985a45b7290ed9acde0ae4501da0a1
Vittu Alexandre <alexandrevittu59@gmail.com>
2018-01-08 14:59:53 +0100

    accueil 30%

---
+++ accueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link type="text/css" rel="stylesheet" href="pages/style.css"/>
+  <title>Transport CCAS</title>
+</head>
+
+<body>
+accueil
+</body>
+<div id="adherent">
+  <fieldset>
+    <legend>Adhérent :</legend>
+      <form action="pages/AjoutAdherent.php">
+        <input type="submit" value="Ajout d'un adhérent">
+      </form>
+      <form action="pages/ListeAdherents.php">
+        <input type="submit" value="Liste des adhérent">
+      </form>
+  </fieldset>
+</div>
+<div id="facturation">
+  <fieldset>
+    <legend>Facturation :</legend>
+  <form action="pages/AjoutAdherent.php">
+      <input type="submit" value="Tarifs">
+  </form>
+  <form action="pages/AjoutAdherent.php">
+      <input type="submit" value="Trimestre">
+  </form>
+  <form action="pages/AjoutAdherent.php">
+      <input type="submit" value="Facturation">
+  </form>
+  <form action="pages/AjoutAdherent.php">
+      <input type="submit" value="Report">
+  </form>
+  </fieldset>
+</div>
+
+</html>
---
+++ pages/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link type="text/css" rel="stylesheet" href="pages/style.css"/>
+  <title>Transport CCAS</title>
+</head>
+
+<body>
+accueil testttt
+</body>
+
+
+</html>
