0057e89686dc5885ea1a9c9f9d757a4020a202e2
Vittu Alexandre <alexandrevittu59@gmail.com>
2018-01-16 15:12:19 +0100

    impressionadd

--- pages/ImpressionDesAdherents.php
+++ pages/ImpressionDesAdherents.php
@@ -16,7 +16,7 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
 
 
   <script language="javascript" type="text/javascript">
