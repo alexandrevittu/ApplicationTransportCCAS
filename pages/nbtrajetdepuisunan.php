7ecd14f9a33167445ede4886d158b568731b3b1d
Vittu Alexandre <alexandrevittu59@gmail.com>
2018-01-23 10:43:44 +0100

    ajout page nb trajet depuis un an

---
+++ pages/nbtrajetdepuisunan.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>nombre de trajet depuis un an</title>
+  <?php
+  include_once "header.php";
+  include_once "../fonctions/fonctions.php";
+  ?>
+</head>
+
+<body>
+</body>
+
+</html>
