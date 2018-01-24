d87888224ae7b959c05ece83b967d01d60f3e631
Salih Gungor <gungor.salih@outlook.fr>
2018-01-24 17:10:16 +0100

    connexion done.

--- pages/connexion.php
+++ pages/connexion.php
@@ -5,31 +5,57 @@
   <link type="text/css" rel="stylesheet" href="style.css"/>
   <title>Transport CCAS</title>
   <meta http-equiv=”refresh” content="5"/>
+  <style>
+    #erreur{
+      display: none;
+    }
+  </style>
 </head>
 <?php
 include_once "header.php";
 include_once "../fonctions/fonctions.php";  //inclut l'en-tete
-
 ?>
+
 <body>
 
-    <form id=connexionForm method="POST" action="verifConnexion.php">
-      <h2 style="text-align:center;">Connexion</h2>
+    <form id=connexionForm method="POST" action="">
+      <h2 style="text-align:center;color:#6FC6E4;">Connexion</h2>
+      <hr class="style-ligne" id="ligne">
       <div class="logoUser">
         <img src="../img/logoUser.png" alt="logo" class="logo">
       </div>
       <div class="saisieUser">
         <label><b>Login</b></label><br>
         <input type="text" placeholder="Enter votre nom d'utilisateur" name="pseudo" required><br>
-
         <label><b>Mot de passe</b></label><br>
         <input type="password" placeholder="Entrer le mot de passe" name="mdp" required><br>
-
+        <div class="alert alert-danger" id="erreur"></div>
         <button type="submit">Se connecter</button>
       </div>
     </form>
     <div class="saisieUser">
     <button type="button" class="btn btn-danger" onclick="history.go(-1);">Retour</button>
     </div>
+    <?php
+    if (isset($_POST['pseudo']) && isset($_POST['mdp'])) {
+
+      $pass_hache = hash('sha256',$_POST['mdp']); // Hachage du mot de passe
+      $pseudo = $_POST['pseudo'];
+      $checkCompte = getCompte($pseudo,$pass_hache);
+
+      if (!$checkCompte)
+      {
+        echo '<style>#erreur{display:block;}</style>';
+        echo '<script>document.getElementById("erreur").innerHTML = "Login ou mot de passe incorrecte.";</script>';
+      }
+      else
+      {
+          session_start();
+          $_SESSION['id'] = $checkCompte['id'];
+          $_SESSION['pseudo'] = $pseudo;
+          header('Location: accueil.php');
+        }
+    }
+    ?>
 </body>
 </html>
--- pages/verifConnexion.php
+++ pages/verifConnexion.php
@@ -1,28 +1,26 @@
 <?php
 include_once '../fonctions/fonctions.php';
-// Hachage du mot de passe
-$pass_hache = hash('sha256',$_POST['mdp']);
-$pseudo = $_POST['pseudo'];
-echo $pass_hache;
-$checkCompte = getCompte($pseudo,$pass_hache);
 
-if (!$checkCompte)
+if (isset($_POST['pseudo']) && isset($_POST['mdp'])) {
 
-{
+  $pass_hache = hash('sha256',$_POST['mdp']); // Hachage du mot de passe
+  $pseudo = $_POST['pseudo'];
+  echo $pass_hache;
+  $checkCompte = getCompte($pseudo,$pass_hache);
 
-    echo 'Mauvais identifiant ou mot de passe !';
-
+  if (!$checkCompte)
+  {
+      echo 'Mauvais identifiant ou mot de passe !';
+  }
+  else
+  {
+      session_start();
+      $_SESSION['id'] = $checkCompte['id'];
+      $_SESSION['pseudo'] = $pseudo;
+      header('accueil.php');
+    }
+}else {
+  echo 'Les variables du formulaires ne sont pas déclarées ..';
 }
 
-else
-
-{
-
-    session_start();
-
-    $_SESSION['id'] = $checkCompte['id'];
-
-    $_SESSION['pseudo'] = $pseudo;
-
-    echo 'Vous êtes connecté !';}
  ?>
