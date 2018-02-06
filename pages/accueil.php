1802d551771659beca2bce0e5da9396dccd9b3ac
Salih <gungor.salih@outlook.fr>
2018-02-06 10:13:18 +0100

    correctif

--- pages/accueil.php
+++ pages/accueil.php
@@ -93,7 +93,7 @@ include_once "../fonctions/fonctions.php";  //inclut l'en-tete
     $recupLigne = unserialize($Nbligne);*/
     if (isset($_SESSION['nbLigne'])) {
       if ($_SESSION['nbLigne'] != 0) {
-        echo '<a id="renouvellementAccueil" href="PubliPostageCSV.php">Il y a '.$_SESSION['nbLigne'].' renouvellement d\'adhesion.</a>';
+        echo '<a id="renouvellementAccueil" href="PubliPostageCSV.php">Il y a '.$_SESSION['nbLigne'].' renouvellement(s) d\'adhesion.</a>';
       }
     }
     ?>
